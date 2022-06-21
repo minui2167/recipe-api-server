b0bd5aea4f64050e8b317111d1e1f69426514ecd
minui2167 <minui2167@gmail.com>
2022-06-21 10:55:56 +0900

    add

--- resources/user.py
+++ resources/user.py
@@ -1,7 +1,7 @@
 import datetime
 from http import HTTPStatus
 from flask import request
-from flask_jwt_extended import create_access_token
+from flask_jwt_extended import create_access_token, get_jwt, jwt_required
 from flask_restful import Resource
 from mysql.connector.errors import Error
 from mysql_connection import get_connection
@@ -148,6 +148,21 @@ class UserLoginResource(Resource):
             connection.close()
             return {'error':str(e)}, 503
 
-        access_token = create_access_token(user_info['id'],  expires_delta = datetime.timedelta(minutes = 1))
+        access_token = create_access_token(user_info['id'],  expires_delta = datetime.timedelta(minutes = 10))
 
         return {'result': 'success', 'access_token ': access_token }, 200
+
+jwt_blacklist = set()
+
+# 로그아웃 기능을 하는 클래스
+class UserLogoutResource(Resource):
+
+    @jwt_required()
+    def post(self):
+
+        jti = get_jwt()['jti']
+        print(jti)
+
+        jwt_blacklist.add(jti)
+
+        return {'result', 'success'}, 200
